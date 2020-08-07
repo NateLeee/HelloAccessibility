c70a86e3425a4cf2e5eb35b0014c758533b67b2d
Nate Lee <nateleee.dev@gmail.com>
2020-08-07 22:01:08 +0800

    - [x] Reading the value of controls.

--- AccessibilityDemo/ContentView.swift
+++ AccessibilityDemo/ContentView.swift
@@ -24,9 +24,21 @@ struct ContentView: View {
     ]
     
     @State private var selectedPicture = Int.random(in: 0...3)
-    
+    @State private var estimate = 25.0
+    @State private var rating = 3
+
+
     var body: some View {
         VStack {
+            Spacer()
+            
+            Stepper("Rate our service: \(rating)/5", value: $rating, in: 1...5)
+                .accessibility(value: Text("\(rating) out of 5"))
+            
+            Slider(value: $estimate, in: 0...50)
+                .padding()
+            .accessibility(value: Text("\(Int(estimate))"))
+            
             VStack {
                 Text("Your score is")
                 
@@ -45,6 +57,7 @@ struct ContentView: View {
             .accessibility(label: Text(labels[selectedPicture]))
             .accessibility(addTraits: .isButton)
             .accessibility(removeTraits: .isImage)
+            
         }
     }
 }
